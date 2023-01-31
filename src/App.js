eaf333fc13e06057df7acda4378839e6e17f15d7
Ashish Verma <ashish.verma@gartner.com>
2023-01-31 14:03:15 +0530

    copy txt update

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React More
+          Learn React with docker
         </a>
       </header>
     </div>
